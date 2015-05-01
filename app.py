225de0afa8929472d0d9c37b21227968aa13f90e
Mihir Pandya <mihir.m.pandya@gmail.com>
2015-05-01 15:33:52 -0400

    Added code for adc in flask app

--- app.py
+++ app.py
@@ -2,14 +2,37 @@ from flask import Flask, render_template
 from flask.ext.socketio import send, emit, SocketIO
 from grideye import Grideye
 from json import dumps
-
 import time, gevent
 
+sys.path.insert(0, 'Adafruit-Raspberry-Pi-Python-Code/Adafruit_ADS1x15/')
+
+from Adafruit_ADS1x15 import ADS1x15
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 ge = Grideye()
+ADS1015 = 0x00  # 12-bit ADC
+adc = ADS1x15(ic=ADS1015)
+
+# Select the gain
+# gain = 6144  # +/- 6.144V
+# gain = 4096  # +/- 4.096V
+# gain = 2048  # +/- 2.048V
+gain = 1024  # +/- 1.024V
+# gain = 512   # +/- 0.512V
+# gain = 256   # +/- 0.256V
+
+# Select the sample rate
+# sps = 8    # 8 samples per second
+# sps = 16   # 16 samples per second
+# sps = 32   # 32 samples per second
+# sps = 64   # 64 samples per second
+# sps = 128  # 128 samples per second
+# sps = 250  # 250 samples per second
+# sps = 475  # 475 samples per second
+sps = 860  # 860 samples per second
+
 sampleArray = [
     list(xrange(10, 18)),
     list(xrange(20, 28)),
@@ -37,6 +60,7 @@ def handle_my_custom_event(data):
         i = i+1
         try:
             arr = ge.getNextArray()
+            volts = adc.readADCSingleEnded(0, gain, sps) / 1000
             emit('updateArray', i % 64)
             gevent.sleep(0.01)
         except Exception as e:
